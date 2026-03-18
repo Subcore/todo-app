9903083fe63d2c1472d428f72af701eb102d5cef
Subcore <fatone7@gmail.com>
2026-03-19 00:03:33 +0700

    2.5 swagger DONE

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,20 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// @title Todo App API
+// @version 1.0
+// @description This is a simple Todo application API.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
 func main() {
 	// Load .env file
 	if err := godotenv.Load(); err != nil {
--- internal/handler/todo_handler.go
+++ internal/handler/todo_handler.go
@@ -25,6 +25,17 @@ type updateTodoRequest struct {
 	Completed bool   `json:"completed"`
 }
 
+// Create godoc
+// @Summary Create a todo
+// @Description Create a new todo item with a title
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Param request body createTodoRequest true "Todo request"
+// @Success 201 {object} model.Todo
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /todos [post]
 func (h *TodoHandler) Create(c *gin.Context) {
 	var req createTodoRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -41,6 +52,17 @@ func (h *TodoHandler) Create(c *gin.Context) {
 	c.JSON(http.StatusCreated, todo)
 }
 
+// Get godoc
+// @Summary Get a todo by ID
+// @Description Retrieve a todo item using its ID
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Param id path uint true "Todo ID"
+// @Success 200 {object} model.Todo
+// @Failure 400 {object} map[string]string
+// @Failure 404 {object} map[string]string
+// @Router /todos/{id} [get]
 func (h *TodoHandler) Get(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.ParseUint(idStr, 10, 32)
@@ -58,6 +80,15 @@ func (h *TodoHandler) Get(c *gin.Context) {
 	c.JSON(http.StatusOK, todo)
 }
 
+// GetAll godoc
+// @Summary Get all todos
+// @Description Retrieve a list of all todo items
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Success 200 {array} model.Todo
+// @Failure 500 {object} map[string]string
+// @Router /todos [get]
 func (h *TodoHandler) GetAll(c *gin.Context) {
 	todos, err := h.svc.GetAllTodos(c.Request.Context())
 	if err != nil {
@@ -68,6 +99,18 @@ func (h *TodoHandler) GetAll(c *gin.Context) {
 	c.JSON(http.StatusOK, todos)
 }
 
+// Update godoc
+// @Summary Update a todo
+// @Description Update title or status of a todo item
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Param id path uint true "Todo ID"
+// @Param request body updateTodoRequest true "Todo update request"
+// @Success 200 {object} model.Todo
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /todos/{id} [put]
 func (h *TodoHandler) Update(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.ParseUint(idStr, 10, 32)
@@ -91,6 +134,17 @@ func (h *TodoHandler) Update(c *gin.Context) {
 	c.JSON(http.StatusOK, todo)
 }
 
+// Delete godoc
+// @Summary Delete a todo
+// @Description Delete a todo item by its ID
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Param id path uint true "Todo ID"
+// @Success 204 "No Content"
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /todos/{id} [delete]
 func (h *TodoHandler) Delete(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.ParseUint(idStr, 10, 32)
