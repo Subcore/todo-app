fcdf58641db082fb1d5b1d35ca7c50f689dcb3b7
Subcore <fatone7@gmail.com>
2026-03-19 10:50:30 +0700

    swagger update

--- internal/model/todo.go
+++ internal/model/todo.go
@@ -2,12 +2,14 @@ package model
 
 import (
 	"time"
+	"gorm.io/gorm"
 )
 
 type Todo struct {
 	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	Title     string    `gorm:"size:255;not null" json:"title"`
 	Completed bool      `gorm:"default:false" json:"completed"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty" swaggertype:"string" example:"2023-10-27T10:00:00Z"`
 }
