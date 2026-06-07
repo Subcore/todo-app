46cf3c047912f1e18a27d6bb53cfee10acde116c
Subcore <fatone7@gmail.com>
2026-06-07 12:47:54 +0700

    chore: upgrade terraform-google-network module to version 10.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 module "vpc" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 9.0"
+  version = "~> 10.0"
 
   project_id   = var.project_id
   network_name = "todo-vpc"
